0d267e9b45d972fee15330417d444a09e0585598
chenguangju <296645438@qq.com>
2018-03-23 09:25:35 +0800

    配置类接口，如果需要增加队列并绑定实现接口和继承默认配置类

--- springboot_rabbitmq_consumer/src/main/java/cn/rabbitmq/config/RabbitConfig.java
+++ springboot_rabbitmq_consumer/src/main/java/cn/rabbitmq/config/RabbitConfig.java
@@ -7,6 +7,7 @@ import org.springframework.amqp.core.Queue;
 public interface RabbitConfig {
 
 		//想自己自定义自己去实现,如果定义多个在自己的config按此类代码多写几套
+		
 		public  Queue queue();
 		public Binding binding(AbstractExchange defaultExchange,Queue queue);
 }
